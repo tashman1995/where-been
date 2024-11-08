93fb0671a3fc19791345e88f7e6b46cc4eb414da
Tom Ashman <57966144+tashman1995@users.noreply.github.com>
2024-11-08 22:12:26 +0000

    feat: display data from BE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { db } from "~/server/db";
 import "~/styles/globals.css";
 
 const mockUrls = [
@@ -13,12 +14,20 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + index} className="w-48">
             <img src={image.url} alt="Hello gallery" />
           </div>
         ))}
