e9e06b64857214877d9cace5c1efb740c0960703
Tom Ashman <57966144+tashman1995@users.noreply.github.com>
2024-11-09 10:25:29 +0000

    feat: display images from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { db } from "~/server/db";
 import "~/styles/globals.css";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
 const mockUrls = [
   "https://utfs.io/f/85EdWt63zPThLkvcwtNchgD4Nb2I3qFs8rOYtk1CvV9Spmud",
@@ -11,26 +11,18 @@ const mockUrls = [
   "https://utfs.io/f/85EdWt63zPThAWDth58nHwGxMfZChP5BiAFSo0beE23LtqIN",
 ];
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.image.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
-  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="w-48">
-            {post.name}
-          </div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        {images.map((image, index) => (
           <div key={image.id + index} className="w-48">
             <img src={image.url} alt="Hello gallery" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
